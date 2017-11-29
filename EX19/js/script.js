c656ae4bff9526b2d4861d74e420204f75932da1
Peng <pwusff@gmail.com>
2017-11-29 14:28:33 -0500

    Modified EX19

--- EX19/js/script.js
+++ EX19/js/script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
 function manipulateDOM() {
     var h1Headers = $('h1');
     h1Headers.text('Hello World');
+    var h3Headers = $('h3');
+    h3Headers.css('color', '#37887D');
+    h3Headers.first().css('text-decoration', 'line-through');
+    var lastH3Header = $('h3:last');
+    lastH3Header.css('background-color', '#53226A');  
 }
 
-var h3Headers = $('h3');
-h3Headers.css('color', '#37887D');
-h3Headers.first().css('text-decoration', 'line-through');
-var lastH3Header = $('h3:last');
-lastH3Header.css('background-color', '#53226A');  
